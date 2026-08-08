6d6f09210e18bb71abdb05c08db15097f82f3fe5
navjottomer <navjottomer@gmail.com>
2026-08-08 07:25:16 +0000

    Declare core compatibility in the theme header
    Adds Requires Shopclass, Tested up to and Requires PHP so the admin can tell
    whether this theme supports the running Shopclass version.

--- index.php
+++ index.php
@@ -8,4 +8,7 @@ Author: Mindstellar
 Author URI: https://github.com/mindstellar
 Widgets: header, footer, item_sidebar
 Theme update URI: storefront
+Requires Shopclass: 6.0.0
+Tested up to: 6.1
+Requires PHP: 8.0
 */
