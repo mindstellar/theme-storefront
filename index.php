96c9281f35b8ac031305e6b759b0fb4880af0a0e
navjottomer <navjottomer@gmail.com>
2026-09-25 07:10:21 +0000

    Storefront 1.3.1

--- index.php
+++ index.php
@@ -3,12 +3,12 @@
 Theme Name: storefront
 Theme URI: https://github.com/mindstellar/theme-storefront
 Description: Storefront — the default Shopclass public theme. Modern, responsive, vanilla-JS classifieds front end.
-Version: 1.3.0
+Version: 1.3.1
 Author: Mindstellar
 Author URI: https://github.com/mindstellar
 Widgets: header, footer, item_sidebar
 Theme update URI: storefront
 Requires Shopclass: 6.0.0
-Tested up to: 6.2
+Tested up to: 6.3
 Requires PHP: 8.0
 */
